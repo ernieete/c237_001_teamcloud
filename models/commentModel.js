e8f35f8a9ad77de1f1f8c3ee2d3030d267a953e8
zhenqi12 <25049650@myrp.edu.sg>
2026-07-21 15:32:31 +0800

    Add community comments and ratings features

---
+++ models/commentModel.js
@@ -0,0 +1,92 @@
+const db = require("../config/db");
+
+// Add a comment
+function addComment(userId, recipeId, comment, callback) {
+    const sql = `
+        INSERT INTO comments (user_id, recipe_id, comment)
+        VALUES (?, ?, ?)
+    `;
+
+    db.query(sql, [userId, recipeId, comment], callback);
+}
+
+// Get all comments for a recipe
+function getCommentsByRecipe(recipeId, callback) {
+    const sql = `
+        SELECT
+            comments.id,
+            comments.user_id,
+            comments.recipe_id,
+            comments.comment,
+            comments.created_at,
+            comments.updated_at,
+            users.username
+        FROM comments
+        JOIN users
+            ON comments.user_id = users.id
+        WHERE comments.recipe_id = ?
+        ORDER BY comments.created_at DESC
+    `;
+
+    db.query(sql, [recipeId], callback);
+}
+
+// Get one comment
+function getCommentById(commentId, callback) {
+    const sql = `
+        SELECT *
+        FROM comments
+        WHERE id = ?
+    `;
+
+    db.query(sql, [commentId], callback);
+}
+
+// Update a comment
+function updateComment(commentId, comment, callback) {
+    const sql = `
+        UPDATE comments
+        SET comment = ?
+        WHERE id = ?
+    `;
+
+    db.query(sql, [comment, commentId], callback);
+}
+
+// Delete a comment
+function deleteComment(commentId, callback) {
+    const sql = `
+        DELETE FROM comments
+        WHERE id = ?
+    `;
+
+    db.query(sql, [commentId], callback);
+}
+
+// Get most discussed recipes
+function getMostDiscussedRecipes(callback) {
+    const sql = `
+        SELECT
+            recipes.id,
+            recipes.title,
+            recipes.image,
+            COUNT(comments.id) AS totalComments
+        FROM recipes
+        LEFT JOIN comments
+            ON recipes.id = comments.recipe_id
+        GROUP BY recipes.id
+        ORDER BY totalComments DESC
+        LIMIT 5
+    `;
+
+    db.query(sql, callback);
+}
+
+module.exports = {
+    addComment,
+    getCommentsByRecipe,
+    getCommentById,
+    updateComment,
+    deleteComment,
+    getMostDiscussedRecipes
+};
--- routes/commentRoutes.js
+++ routes/commentRoutes.js
@@ -1,5 +1,34 @@
 const express = require("express");
-
 const router = express.Router();
+
+const commentController = require("../controllers/commentController");
+const { requireLogin } = require("../middleware/authMiddleware");
+
+// Add comment
+router.post(
+    "/add",
+    requireLogin,
+    commentController.addComment
+);
+
+// Edit own comment
+router.post(
+    "/edit",
+    requireLogin,
+    commentController.updateComment
+);
+
+// Delete own comment or admin delete
+router.post(
+    "/delete",
+    requireLogin,
+    commentController.deleteComment
+);
+
+// Most discussed recipes
+router.get(
+    "/most-discussed",
+    commentController.showMostDiscussed
+);
 
 module.exports = router;
